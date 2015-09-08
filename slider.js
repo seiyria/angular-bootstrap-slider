62f9ff55e91b12f9e9f9f7de3da0b1f3f1e01a0f
jaml <dev@jaml.site>
2015-09-08 09:12:30 +0100

    Add the possibility to set "ticks-snap-bounds" attribute in the directive

--- slider.js
+++ slider.js
@@ -16,6 +16,7 @@ angular.module('ui.bootstrap-slider', [])
                 sliderid: '=',
                 ticks: '=',
                 ticksLabels: '=',
+                ticksSnapBounds: '=',
                 scale: '=',
                 formatter: '&',
                 onStartSlide: '&',
@@ -55,6 +56,7 @@ angular.module('ui.bootstrap-slider', [])
                     setOption('tooltipseparator', attrs.tooltipseparator, ':');
                     setOption('ticks', $scope.ticks);
                     setOption('ticks_labels', $scope.ticksLabels);
+                    setOption('ticks_snap_bounds', $scope.ticksSnapBounds);
                     setOption('scale', $scope.scale, 'linear');
 
                     setFloatOption('min', $scope.min, 0);
