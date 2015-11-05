094762d13f6432fd215aa8e4b33451c3595af5fe
JulianWielga <j@touk.pl>
2015-11-05 15:55:12 +0100

    better fix

--- slider.js
+++ slider.js
@@ -65,7 +65,8 @@ angular.module('ui.bootstrap-slider', [])
                     setFloatOption('max', $scope.max, 10);
                     setFloatOption('step', $scope.step, 1);
                     var strNbr = options.step + '';
-                    var decimals = strNbr.substring(strNbr.lastIndexOf('.') + 1 || strNbr.length);
+                    var dotPos = strNbr.search(/[^.,]*$/);
+                    var decimals = strNbr.substring(dotPos).length;
                     setFloatOption('precision', attrs.precision, decimals);
 
                     setBooleanOption('tooltip_split', attrs.tooltipsplit, false);
